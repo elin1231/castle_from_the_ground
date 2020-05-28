19c31a869ccd39af2d6cc13b8148c53f7ba54952
elin1231 <43862267+elin1231@users.noreply.github.com>
2020-05-28 17:59:49 -0500

    Update historic_data_processing.py

--- crawlers/historic_data_processing.py
+++ crawlers/historic_data_processing.py
@@ -1,6 +1,7 @@
 from pandas_datareader import data as pdr
 import pandas as pd
 import os
+import yfinance as yf
 
 
 historial_data_path = "./output/historical_data"
@@ -22,6 +23,29 @@ def add_mean(df):
         df["Average"] = mean
 
 
+def add_general_info(df, ticker):
+    insert_columns = [
+        "zip",
+        "sector",
+        "city",
+        "state",
+        "industry",
+        "exchange",
+    ]
+
+    for column in insert_columns:
+        if column not in df.columns:
+            try:
+                company_info = yf.Ticker(ticker).info
+                if company_info[column]:
+                    df[column] = company_info[column]
+                else:
+                    df[column] = "NA"
+            except:
+                print("Failed: {}".format(ticker))
+                continue
+
+
 if __name__ == "__main__":
     remove_empty_files()
     for file in os.listdir(historial_data_path):
@@ -29,10 +53,11 @@ if __name__ == "__main__":
             os.path.join(historial_data_path, file)
         ):
             print(file)
+            ticker = file.split(".")[0]
             with open(os.path.join(historial_data_path, file), "r") as csv_file:
                 df = pd.read_csv(csv_file)
                 # Add whatever method you want to do for historical data, all should be in their own method
                 add_mean(df)
-                # add_general_info(df, insert_columns)
+                add_general_info(df, ticker)
                 #
-                df.to_csv(os.path.join(historial_data_path, file))
+                df.to_csv(os.path.join(historial_data_path, file), index=False)
